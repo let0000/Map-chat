7f8d58bf45be29a9e3b6d679e10203a5ae10c9e6
JINHO LEE <wlsgh8309@naver.com>
2023-03-14 16:59:50 +0900

    Geolocation을 이용해서 현재 위치의 경위도 값 받아오는 기능

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import KakaoMap from "../components/KakaoMap";
 import "./Home.css";
 
 export default function Home() {
+  const [locationError, setLocationError] = useState(false);
+  const [location, setLocation] = useState({
+    lat: null,
+    lon: null,
+  });
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // 위치 정보 가져오기에 성공한 경우
+        console.log(position);
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        setLocationError(false);
+      },
+      (error) => {
+        // 위치 정보 가져오기에 실패한 경우
+        console.log(error);
+        setLocationError(true);
+      }
+    );
+  }, [locationError]);
+
   return (
     <div>
+      {locationError ? (
+        <p>
+          위치 정보가 활성화되어 있지 않습니다. 위치정보를 활성화 한 후
+          새로고침해주세요
+        </p>
+      ) : (
+        <div>
+          <p>Lat: {location.lat}</p>
+          <p>Lat: {location.lon}</p>
+        </div>
+      )}
+
       <KakaoMap />
     </div>
   );
